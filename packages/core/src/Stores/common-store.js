2cbd74c484ebbf825a5ce89d57cef396101e924c
Matin shafiei <matin@regentmarkets.com>
2025-12-31 11:05:31 +0800

    fix: Removed balance call before language switching mechanism (#554)

--- packages/core/src/Stores/common-store.js
+++ packages/core/src/Stores/common-store.js
@@ -1,18 +1,20 @@
-import * as SocketCache from '_common/base/socket_cache';
 import { action, makeObservable, observable } from 'mobx';
-import { getAllowedLanguages, getInitialLanguage } from '@deriv-com/translations';
+
 import {
-    UNSUPPORTED_LANGUAGES,
     initMoment,
-    setLocale,
     isMobile,
-    routes,
-    toMoment,
     mapErrorMessage,
+    routes,
+    setLocale,
+    toMoment,
+    UNSUPPORTED_LANGUAGES,
 } from '@deriv/shared';
+import { getAllowedLanguages, getInitialLanguage } from '@deriv-com/translations';
+
 import BaseStore from './base-store';
+
 import ServerTime from '_common/base/server_time';
-import WS from 'Services/ws-methods';
+import * as SocketCache from '_common/base/socket_cache';
 
 export default class CommonStore extends BaseStore {
     constructor(root_store) {
@@ -106,29 +108,24 @@ export default class CommonStore extends BaseStore {
         if (key === 'EN') {
             window.localStorage.setItem('i18n_language', key);
         }
-        await WS?.wait('balance');
-        return new Promise((resolve, reject) => {
-            WS.setSettings({
-                set_settings: 1,
-                preferred_language: key,
-            }).then(async () => {
-                const new_url = new URL(window.location.href);
-                if (key === 'EN') {
-                    new_url.searchParams.delete('lang');
-                } else {
-                    new_url.searchParams.set('lang', key);
-                }
-                window.history.pushState({ path: new_url.toString() }, '', new_url.toString());
-                try {
-                    await initMoment(key);
-                    await setLocale(key);
-                    this.changeCurrentLanguage(key);
-                    resolve();
-                } catch (e) {
-                    reject();
-                }
-            });
-        });
+
+        // Update URL with language parameter
+        const new_url = new URL(window.location.href);
+        if (key === 'EN') {
+            new_url.searchParams.delete('lang');
+        } else {
+            new_url.searchParams.set('lang', key);
+        }
+        window.history.pushState({ path: new_url.toString() }, '', new_url.toString());
+
+        // Update i18n and moment locale
+        try {
+            await initMoment(key);
+            await setLocale(key);
+            this.changeCurrentLanguage(key);
+        } catch (e) {
+            return Promise.reject(e);
+        }
     };
 
     setAppstorePlatform(platform) {
